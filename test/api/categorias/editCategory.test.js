8af27bda6976f0c14857e0b6f58350774b08e8ff
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-10-13 17:02:50 -0300

    editCategory test

---
+++ test/api/categorias/editCategory.test.js
@@ -0,0 +1,42 @@
+const {spec, request} = require('pactum')
+
+request.setBaseUrl('http://lojaebac.ebaconline.art.br')
+
+let id 
+let token;
+
+
+beforeEach(async () => {
+    token = await spec()
+    .post("/public/authUser")
+    .withJson({
+        "email": "admin@admin.com",
+        "password": "admin123"
+    })
+    .returns('data.token')
+});
+
+
+beforeEach( async () => {
+    id = await spec()
+    .post("/api/addCategory")
+    .withJson({
+         "authorization": `${token}`,
+         "name": "celular" 
+    })
+    .returns('data._id')
+});
+
+it.only('API - deve editar uma categoria com succeso', async () => {
+    await spec()
+    .put(`/api/editCategory/${id}`)
+    .withJson({
+         "authorization": `${token}`,
+         "name": "celular editado" 
+        })
+        .expectStatus(200)
+        .expectJsonLike({
+            success: true,
+            message: "category updated",
+        })
+});
