65bf188d1826b023d6ec18d85e365e730f9ad024
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-10-13 15:15:16 -0300

    login.teste

--- test/api/login.test.js
+++ test/api/login.test.js
@@ -1,1 +1,33 @@
 const { spec } = require('pactum')
+
+
+it('API - deve autenticar o usuario corretamente', async () => {
+    await spec()
+    .post("http://lojaebac.ebaconline.art.br/public/authUser")
+    .withJson({
+         "email": "admin@admin.com",
+         "password": "admin123"
+    })
+    .expectStatus(200)
+    .expectJson('success', true)
+});
+
+it('API - deve autenticar o usuario corretamente com profile', async () => {
+    await spec()
+    .post("http://lojaebac.ebaconline.art.br/public/authUser")
+    .withJson({
+        "email": "admin@admin.com",
+        "password": "admin123"
+    })
+    .expectStatus(200)
+    .expectJsonLike({
+        success: true,
+        message: "login successfully",
+        data: {
+            role: "admin",
+            profile: {
+                firstName: "admin",
+            }
+        }
+    })
+});
